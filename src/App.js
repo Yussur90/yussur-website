0543be1e55e5c730c8e15948826f0548c98cbf53
Yussur alani <36266815+Yussur90@users.noreply.github.com>
2021-12-16 00:48:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello this is Yussur Alani</p>
+        <p>Hello this is Yussur Alani :)</p>
       </header>
     </div>
   );
