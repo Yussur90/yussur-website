f0975b1115e94b280c8b5ae973c9629e40ff470f
Yussur alani <36266815+Yussur90@users.noreply.github.com>
2021-12-16 00:44:05 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello this is Yussur Alani</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
