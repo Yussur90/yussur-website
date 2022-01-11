1635f7114028b06c9add6f45aa04457545da8fd4
Yussur alani <36266815+Yussur90@users.noreply.github.com>
2022-01-11 17:48:11 +0200

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    console.log("loo");
+    axios.get("http://localhost:3031/posts/list").then((res) => {
+      console.log(res);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p
+        // onClick={() => setYussur()}
+        >
+          Hello this is Yussur Alani
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
