6c5d1fe55a1e9ec8b5f9c8f526ea27020799bd21
Yussur alani <36266815+Yussur90@users.noreply.github.com>
2022-01-11 17:53:13 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+let config = {
+  dbUrl:
+    "mongodb+srv://yussur:yuyu541990@cluster0.zdjfl.mongodb.net/yussur?retryWrites=true&w=majority",
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+const port = 3031;
+const config = require("./config");
+
+const postsRouter = require("./routes/posts");
+
+app.use(logger("dev"));
+
+const dbUrl = process.env.DATABASE_URL || config.dbUrl;
+
+var options = {
+  keepAlive: 1,
+  connectTimeoutMS: 30000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(dbUrl, options, (err) => {
+  if (err) console.log(err);
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/posts", postsRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
+
+app.listen(port, function () {
+  console.log("Runnning on " + port);
+});
+module.exports = app;
