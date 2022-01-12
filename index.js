fff732796e98aa43f7450bf552108df45c3d4f7f
Yussur alani <36266815+Yussur90@users.noreply.github.com>
2022-01-12 18:00:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,12 +36,11 @@ app.use(bodyParser.json());
 app.use("/posts", postsRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static( 'client/build' ));
 
-  app.get("*", (req, res) => {
-    console.log("hello")
-    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
-  });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
 }
 
 app.listen(port, function () {
